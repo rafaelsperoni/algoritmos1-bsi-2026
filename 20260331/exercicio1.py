381d567d79e5043df5ff61985a92c163231d05e1
Rafael de Moura Speroni <rafaelsperoni@gmail.com>
2026-03-31 11:51:18 -0300

    Exercicios 31/03

---
+++ 20260331/exercicio1.py
@@ -0,0 +1,8 @@
+nome = input("Informe o nome: ") #atribuicao
+curso = input("Informe o curso: ")  #atribuição com entrada
+
+#instrução de saída com variável
+print("Resultados")
+print("O nome armazenado é ", nome, "!")
+#saída formatada com variável
+print(f"Meu curso é o {curso} e eu tô curtindo!")
---
+++ 20260331/exercicio2.py
@@ -0,0 +1,18 @@
+#ENTRADAS
+#usuario informa dois numeros x e y
+x = float(input("Informe o primeiro valor: "))
+y = int(input("Informe o segundo valor: "))
+
+#PROCESSAMENTO
+#a partir de x e y, calulcar a soma
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x / y
+
+#SAIDAS
+#exibir a soma calculada
+print(f"A soma dos valores é {soma}")
+print(f"A subtração é {subtracao}")
+print(f"A multiplicação é {multiplicacao}")
+print(f"A divisão é {divisao}")
